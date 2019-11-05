7377d9797cf0f0c13a85eb6ac22a6adf51be0147
vaishnavi-2503 <pkrishnavaishnavi25@gmail.com>
2019-11-04 19:06:27 -0500

    add poll data and display it

--- venv/poll.py
+++ venv/poll.py
@@ -1,10 +1,15 @@
 from flask import Flask,render_template
 import os
+poll_data={
+    'question':'which web framework do you use?',
+    'fields':['Flask', 'Django', 'TurboGears', 'web2py', 'pylonsproject']
+    }
+
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('poll.html')
+    return render_template('poll.html',data=poll_data)
 
 if __name__ == "__main__":
     app.run(debug=True)    
